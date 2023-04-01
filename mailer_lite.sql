2edc6f3b161c434c017c5928366d03d7b2ff5f31
Cecilio Morales <cecilio.morales@leniolabs.com>
2023-04-01 10:19:56 -0500

    SQL File

---
+++ mailer_lite.sql
@@ -0,0 +1,7 @@
+CREATE TABLE `api_key` (
+  `id` smallint unsigned NOT NULL AUTO_INCREMENT,
+  `api_key` text COLLATE utf8mb4_unicode_ci NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci
